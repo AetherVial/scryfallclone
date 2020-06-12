32ee2ade1bd2f7aa9148cf9dce69b93efdfabe03
Stanton Huang <stantonhuang11@gmail.com>
2020-06-11 23:09:35 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
+import catto from './frostcatto1.png';
 import './App.css';
 
 function App() {
+  let [searchTerm, setSearchTerm] = useState('');
+
+
+  function onFormSubmit(e) {
+    e.preventDefault();
+    alert(searchTerm);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={catto} className="App-logo" alt="logo" />
+
+        <form onSubmit={e => onFormSubmit(e)}>
+          <input type="text" placeholder="Start Searching Here!" onChange={e => setSearchTerm(e.target.value.toLowerCase())}></input>
+        </form>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>Welcome to ScraureusFetch, where Aureus fetches cardboard rectangles.</code>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
